7b100a6b51ad9d40054dc87e316b9184e15bc4a4
JoshT <wunjosh18@gmail.com>
2022-06-17 12:22:57 +0800

    Added error function

--- T_Matrix_Multiplication/sources/utils.c
+++ T_Matrix_Multiplication/sources/utils.c
@@ -1,5 +1,15 @@
 #include "../includes/my_matrix.h"
 
+size_t  ft_strlen(char  *str)
+{
+    int i;
+
+    i = 0;
+    while (str[i])
+        i++;
+    return (i);
+}
+
 void    *ft_calloc(size_t n_elem, size_t t_size)
 {
     void    *ret;
@@ -15,4 +25,13 @@ void    *ft_calloc(size_t n_elem, size_t t_size)
         i++;
     }
     return (ret);
+}
+
+void    err_msg(char *str)
+{
+    write(2, "Error message: ", 15);
+    write(2, str, ft_strlen(str));
+    write(2, "\n", 1);
+    perror("Error description: ");
+    exit(EXIT_FAILURE);
 }
