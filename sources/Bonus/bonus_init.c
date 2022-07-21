180462df9a6b9a82cb524f79d9766c301c451ce2
Josh Thor <wunjosh@gmail.com>
2022-07-21 19:48:54 +0800

    minor mod

--- sources/Bonus/bonus_init.c
+++ sources/Bonus/bonus_init.c
@@ -28,13 +28,13 @@ ssize_t	ft_decide_scale(size_t size)
 
 ssize_t	ft_decide_peak(size_t size)
 {
-	if (size <= 100)
+	if (size <= 1000)
 		return (5);
-	else if (size <= 500)
-		return (4);
-	else if (size <= 1000)
-		return (3);
 	else if (size <= 5000)
+		return (4);
+	else if (size <= 7500)
+		return (3);
+	else if (size <= 10000)
 		return (2);
 	else
 		return (1);
